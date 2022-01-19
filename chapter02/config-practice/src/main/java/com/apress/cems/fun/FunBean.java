524e9f5ee8c42caea51c3ac0e55da23747a36e07
Eduardo Ortega <eduardo.ortega@edataconsulting.de>
2022-01-19 13:34:22 +0000

    Do page 129 excercise

--- chapter02/config-practice/src/main/java/com/apress/cems/fun/FunBean.java
+++ chapter02/config-practice/src/main/java/com/apress/cems/fun/FunBean.java
@@ -41,8 +41,7 @@ import javax.annotation.PreDestroy;
  * @since 1.0
  * Bean that is initialized using all three techniques supported in Spring
  */
-// TODO 12. Add initialization and destroy methods to implement all three techniques specified in the book
-public class FunBean /*implements InitializingBean, DisposableBean*/ {
+public class FunBean implements InitializingBean, DisposableBean {
     private Logger logger = LoggerFactory.getLogger(FunBean.class);
 
     private DepBean depBean;
@@ -57,5 +56,31 @@ public class FunBean /*implements InitializingBean, DisposableBean*/ {
         this.depBean = depBean;
     }
 
-    // ..
+    @PostConstruct
+    private void init() {
+        logger.info("Stage 3: Calling @PostConstruct");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        logger.info("Stage 4: Calling afterPropertiesSet");
+    }
+
+    private void beanInitialization() {
+        logger.info("Stage 5: Calling @Bean(initMethod)");
+    }
+
+    @PreDestroy
+    private void preDestroy() {
+        logger.info("Stage 6: Calling preDestroy");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        logger.info("Stage 7: Calling destroy");
+    }
+
+    private void beanDestruction() {
+        logger.info("Stage 8: Calling @Bean(destroyMethod)");
+    }
 }
